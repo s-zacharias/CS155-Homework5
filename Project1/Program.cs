4afe2c4fe92cfd0b15e131e95407229dc19f2eee
Stephanie Zacharias <stephanie@stephaniesmbp4.attlocal.net>
2020-10-05 18:48:17 -0500

    first commit

---
+++ Project1/Program.cs
@@ -0,0 +1,45 @@
+﻿/// Homework 5		Project 1
+/// File Name: Program.cs
+/// @author: Stephanie Zacharias
+/// Date:  October 5, 2020
+///
+/// Problem Statement: Write code to creat a copy of an array.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Create initial array with 10 items
+/// 2) Assign values to the array
+/// 3) Create a second array empty array of the same length
+/// 4) Use a loop to copy values from the first array into the second
+/// 5) Print out both arrays
+///
+
+using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create initial array and assign values to the 10 items in the array
+            int[] firstArray = new int[10] { 5, 9, 0, 7, 12, 45, 23, 2, 16, 89 };
+
+            // create second empty array
+            int[] secondArray = new int[firstArray.Length];
+
+            // assign the values from the first array to elements in the second array
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                secondArray[i] = firstArray[i];
+            }
+
+            // print out both arrays
+            Console.WriteLine("First Array: ");
+            Console.WriteLine(string.Join("\n", firstArray));
+            Console.WriteLine("\nSecond Array: ");
+            Console.WriteLine(string.Join("\n", secondArray));
+           
+        }
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = GenerateNumbers();
+            Reverse(numbers);
+            PrintNumbers(numbers);
+        }
+
+        static int[] GenerateNumbers()
+        {
+            int[] result = new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            return result;
+        }
+
+        static int[] Reverse(int[] arrayToReverse)
+        {
+            int temp;
+
+            for (int i = 0; i < arrayToReverse.Length; i++)
+            {
+                temp = arrayToReverse[i];
+                arrayToReverse[i] = arrayToReverse[arrayToReverse.Length - i - 1];
+                arrayToReverse[arrayToReverse.Length - i - 1] = temp;
+            }
+
+            return arrayToReverse;
+        }
+
+        static void PrintNumbers(int[] arrayToPrint)
+        {
+            foreach (int num in arrayToPrint)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
